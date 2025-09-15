b7245dc387ef5a89cc3e719c927c481b1f1be480
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2025-09-15 12:24:10 +0530

    feat: added handlers logic

--- internal/server/http.go
+++ internal/server/http.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -32,9 +33,49 @@ type ConsumeResponse struct {
 	Record Record `json:"record"`
 }
 
-func (s *httpServer) handleProduce(w http.ResponseWriter, r *http.Request){}
+func (s *httpServer) handleProduce(w http.ResponseWriter, r *http.Request){
+	var req ProduceRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w,err.Error(), http.StatusBadRequest)
+		return
+	}
 
-func (s *httpServer) handleConsume(w http.ResponseWriter, r *http.Request) {}
+	offset,err := s.Log.Append(req.Record)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	resp := ProduceResponse{Offset: offset}
+	err = json.NewEncoder(w).Encode(resp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+	}
+}
+
+func (s *httpServer) handleConsume(w http.ResponseWriter, r *http.Request) {
+	var req ConsumeRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	record,err := s.Log.Read(req.Offset)
+	if err == ErrOffsetNotFound {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	resp := ConsumeResponse{Record: record}
+	err = json.NewEncoder(w).Encode(resp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
 
 func NewHTTPServer(addr string) *http.Server {
 	httpSrv := newHTTPServer()
